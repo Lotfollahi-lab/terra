84e728c126eac6f2e76d4d904259e4a5902144c5
Adam Boxall <ab70@sanger.ac.uk>
2024-05-15 12:58:56 +0100

    add docs for filter_poor_quality_cells

--- src/nichejepa/preprocess.py
+++ src/nichejepa/preprocess.py
@@ -3,7 +3,21 @@ import numpy as np
 
 
 def filter_poor_quality_cells(adata: anndata.AnnData) -> anndata.AnnData:
-    """Filter cells that do not pass QC."""
+    """
+    Filter cells that do not pass QC
+
+    Filter cells based on the "filter_pass" field in `adata.obs`.
+
+    Parameters
+    --------
+    adata : anndata.AnnData
+        An AnnData object containing a QC field in `adata.obs["filter_pass"]`
+
+    Returns
+    --------
+    adata : anndata.AnnData
+        A filtered AnnData object.
+    """
 
     if "filter_pass" in adata.obs.columns:
         filter_pass_idx = np.where([filter_pass == 1 for filter_pass in adata.obs["filter_pass"]])[0]
