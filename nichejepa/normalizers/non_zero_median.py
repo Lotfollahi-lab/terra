084160f914a508d4266c96fa7bf309e8a8553d09
Adam Boxall <ab70@sanger.ac.uk>
2024-05-28 17:25:15 +0100

    add tests for non_zero_median

--- nichejepa/normalizers/non_zero_median.py
+++ nichejepa/normalizers/non_zero_median.py
@@ -7,9 +7,7 @@ import numpy as np
 import scipy
 
 
-def non_zero_median(x: scipy.sparse.csr_matrix,
-                    gene_nzmedians_file: Path | str,
-                    probed_genes: np.ndarray) -> scipy.sparse.csr_matrix:
+def non_zero_median(x: scipy.sparse.csr_matrix) -> scipy.sparse.csr_matrix:
     """
     Normalize by non-zero median expression.
 
@@ -19,10 +17,6 @@ def non_zero_median(x: scipy.sparse.csr_matrix,
     ----------
     x: scipy.sparse.csr_matrix
         A sparse matrix where each row represents an observation and each column represents a feature.
-    gene_nzmedians_file: Path | str
-        Path to pickle file containing dictionary of non-zero median gene expression values.
-    probed_genes:
-        Array with ensembl ids of probed genes.
 
     Returns
     ----------
@@ -30,14 +24,8 @@ def non_zero_median(x: scipy.sparse.csr_matrix,
         A sparse matrix containing the normalized features.
     """
 
-    # Load dictionary of gene non-zero medians
-    with open(gene_nzmedians_file, "rb") as f:
-        gene_nzmedians_dict = pickle.load(f)
+    gene_non_zero_medians = [np.median(x[:, i][np.nonzero(x[:, i])]) for i in range(x.shape[1])]
 
-    # Retrieve gene non-zero medians
-    gene_nzmedians = np.array([gene_nzmedians_dict[gene_id] for gene_id in probed_genes])
-
-    # Normalize counts
-    y = x / gene_nzmedians
+    y = x / gene_non_zero_medians
 
     return y
