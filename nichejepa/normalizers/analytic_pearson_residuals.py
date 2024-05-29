f57b43e53d37e4d98815fd522621085057233734
Adam Boxall <ab70@sanger.ac.uk>
2024-05-29 13:28:32 +0100

    add test for analytic_pearson_residuals

--- nichejepa/normalizers/analytic_pearson_residuals.py
+++ nichejepa/normalizers/analytic_pearson_residuals.py
@@ -12,18 +12,19 @@ def analytic_pearson_residuals(x: scipy.sparse.csr_matrix, theta: float = 100) -
     normalization of single-cell RNA-seq UMI data. Genome Biol. 22, 258 (2021)". Residuals are based on a negative
     binomial offset model with overdispersion shared across genes. Residuals are clipped to 'sqrt(n_obs)'. Negative
     residuals for a cell and gene indicate that fewer counts are observed than expected, compared to the gene’s average
-    expression and cell read depth. Positive residuals indicate more counts than expected.
+    expression and cell read depth. Positive residuals indicate more counts than expected. By default, overdispersion
+    `theta=100` is used.
 
     The implementation is based on
-    https://github.com/scverse/scanpy/blob/4642cf8e2e51b257371792cb4fcb9611c0a81123/scanpy/experimental/pp/_normalization.py#L36
-    (03.05.2024).
+    https://github.com/scverse/scanpy/blob/4642cf8e2e51b257371792cb4fcb9611c0a81123/scanpy/experimental/pp/_normalization.py#L36.
 
     Parameters
     ----------
     x: scipy.sparse.csr_matrix
-        A sparse matrix where each row represents an observation and each column represents a feature.
+        A sparse matrix where each row represents an observation and each column represents a feature,
+        containing raw counts as features (i.e. not scaled or normalized).
     theta: float
-        The overdispersion parameter.
+        The overdispersion parameter, defaults to 100.
 
     Returns
     ----------
@@ -31,12 +32,18 @@ def analytic_pearson_residuals(x: scipy.sparse.csr_matrix, theta: float = 100) -
         A sparse matrix containing the normalized features.
     """
 
-    sum_counts_cells = np.sum(x, axis=1).reshape(-1, 1)
-    sum_counts_genes = np.sum(x, axis=0).reshape(1, -1)
-    sum_counts_total = np.sum(sum_counts_genes)
-    mu_counts = np.array(sum_counts_cells @ sum_counts_genes / sum_counts_total)
-    diff_counts = np.array(x - mu_counts)
-    residuals_counts = diff_counts / np.sqrt(mu_counts + mu_counts ** 2 / theta)
-    y = np.clip(residuals_counts, a_min=-np.sqrt(x.shape[0]), a_max=np.sqrt(x.shape[0]))
+    if theta <= 0:
+        raise ValueError("Pearson residuals require theta > 0")
+
+    cell_sums = np.sum(x, axis=1).reshape(-1, 1)
+    gene_sums = np.sum(x, axis=0).reshape(1, -1)
+    sum_total = np.sum(gene_sums)
+
+    mu = np.array(cell_sums @ gene_sums / sum_total)
+    diff = np.array(x - mu)
+    residuals = diff / np.sqrt(mu + mu ** 2 / theta)
+
+    clip = np.sqrt(x.shape[0])
+    y = np.clip(residuals, a_min=-clip, a_max=clip)
 
     return y
