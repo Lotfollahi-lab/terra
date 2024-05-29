ef283d8352ef79bca2a19c2df9c70bcda40e1fe6
Adam Boxall <ab70@sanger.ac.uk>
2024-05-29 13:31:44 +0100

    simplify

--- nichejepa/normalizers/shifted_log_mean.py
+++ nichejepa/normalizers/shifted_log_mean.py
@@ -7,24 +7,17 @@ import numpy as np
 import scanpy as sc
 import scipy
 
+from nichejepa.normalizers import mean_normalize_by_gene, shifted_log
 
-def shifted_log_mean(x: scipy.sparse.csr_matrix,
-                     gene_logmeans_file: Path | str,
-                     probed_genes: np.ndarray) -> scipy.sparse.csr_matrix:
+
+def shifted_log_mean(x: scipy.sparse.csr_matrix) -> scipy.sparse.csr_matrix:
     """
-    Normalize by shifted log and log mean gene expression.
-
-    Implements normalization using `sc.pp.log1p`. This function should be applied following normalization per cell by
-    cell area or read depth.
+    Applies shifted log normalization followed by mean normalization by gene.
 
     Parameters
     ----------
     x: scipy.sparse.csr_matrix
         A sparse matrix where each row represents an observation and each column represents a feature.
-    gene_logmeans_file: Path | str
-        Path to pickle file containing dictionary of log mean gene expression values.
-    probed_genes:
-        Array with ensembl ids of probed genes that will be normalized.
 
     Returns
     ----------
@@ -32,14 +25,7 @@ def shifted_log_mean(x: scipy.sparse.csr_matrix,
         A sparse matrix containing the normalized features.
     """
 
-    y = sc.pp.log1p(x)
+    log_normalized = shifted_log(x)
+    mean_log_normalized = mean_normalize_by_gene(log_normalized)
 
-    # Load dictionary of gene logmeans
-    with open(gene_logmeans_file, "rb") as f:
-        gene_logmeans_dict = pickle.load(f)
-
-    gene_logmeans = np.array([gene_logmeans_dict[gene_id] for gene_id in probed_genes])
-
-    y = y / gene_logmeans
-
-    return y
+    return mean_log_normalized
