36b7ef979c0fabc7f8aac0a7ebf084d19153f4f5
arpitdm <arpitdm@gmail.com>
2024-08-15 14:35:12 +0100

    rename: separate_cell_and_neighborhood_tokens is renamed to use_separate_cell_and_neighborhood_tokens for clarity.

--- src/nichejepa/tokenizers/cell_neighborhood_rank_tokenizer.py
+++ src/nichejepa/tokenizers/cell_neighborhood_rank_tokenizer.py
@@ -103,7 +103,7 @@ class CellNeighborhoodRankTokenizer:
                  neighborhood_gene_nzmeans_file: Path | str = NEIGHBORHOOD_GENE_NZMEANS_FILE,
                  neighborhood_gene_logmeans_file: Path | str = NEIGHBORHOOD_GENE_LOGMEANS_FILE,
                  token_dictionary_file: Path | str = TOKEN_DICTIONARY_FILE,
-                 separate_cell_and_neighborhood_tokens: bool=False,
+                 use_separate_cell_and_neighborhood_tokens: bool=False,
                  cell_special_tokens: Optional[list[str]] = None, # ["<cls_cell>"],
                  cell_special_tokens_idx: Optional[list[int]] = None, # [0],
                  neighborhood_special_tokens: Optional[list[str]] = None, # ["<cls_neighborhood>"],
@@ -153,7 +153,7 @@ class CellNeighborhoodRankTokenizer:
             Only relevant if 'norm_method' in ['shifted_logmean'].
         token_dictionary_file:
             Path to pickle file containing token dictionary (gene tokens are Ensembl IDs + '_cell' or '_neighborhood').
-        separate_cell_and_neighborhood_tokens:
+        use_separate_cell_and_neighborhood_tokens:
             If 'True', separate cell and neighborhood gene tokens are used.
         cell_special_tokens:
             List with special tokens inserted into the gene token vector containing cell gene tokens.
@@ -177,7 +177,7 @@ class CellNeighborhoodRankTokenizer:
         self.neighborhood_gene_means_file = neighborhood_gene_means_file
         self.neighborhood_gene_nzmeans_file = neighborhood_gene_nzmeans_file
         self.neighborhood_gene_logmeans_file = neighborhood_gene_logmeans_file
-        self.separate_cell_and_neighborhood_tokens = separate_cell_and_neighborhood_tokens
+        self.use_separate_cell_and_neighborhood_tokens = use_separate_cell_and_neighborhood_tokens
         self.cell_special_tokens = cell_special_tokens
         self.cell_special_tokens_idx = cell_special_tokens_idx
         self.neighborhood_special_tokens = neighborhood_special_tokens
@@ -189,7 +189,7 @@ class CellNeighborhoodRankTokenizer:
 
         # Get vocabulary and gene Ensembl IDs (protein-coding and miRNA genes)
         self.vocab = list(self.token_dict.keys())
-        if self.separate_cell_and_neighborhood_tokens:
+        if self.use_separate_cell_and_neighborhood_tokens:
             self.coding_miRNA_ids = [key.split("_")[0] for key in list(self.vocab) if "_cell" in key]
         else:
             self.coding_miRNA_ids = [key for key in list(self.vocab) if "ENS" in key]
@@ -371,7 +371,7 @@ class CellNeighborhoodRankTokenizer:
             [self.coding_miRNA_dict.get(gene_id, False) for gene_id in adata.var["ensembl_id"]]
             )[0]
         coding_miRNA_ids = adata.var["ensembl_id"][coding_miRNA_idx]
-        if self.separate_cell_and_neighborhood_tokens:
+        if self.use_separate_cell_and_neighborhood_tokens:
             coding_miRNA_tokens_cell = np.array([self.token_dict[gene_id + "_cell"] for gene_id in coding_miRNA_ids])
             coding_miRNA_tokens_neighborhood = np.array(
                 [self.token_dict[gene_id + "_neighborhood"] for gene_id in coding_miRNA_ids])
