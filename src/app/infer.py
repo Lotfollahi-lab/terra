Fix gene transformers, masking and cell datasets to enable count, rank or combined gt type

src/app/infer.py
@@ -319,27 +319,25 @@ def infer(args: dict,
     all_neighborhood_gene_emb_dict = {}
 
     for itr, (udata, _, _, masks_attention) in tqdm(enumerate(loader)):
-        # Load gene tokens and segmentation label to the specified device
-        tokens = udata[0].to(device, non_blocking=True)
-        segments = udata[1].to(device, non_blocking=True)
-        positions = udata[2].to(device, non_blocking=True)
-        counts = udata[3].to(device, non_blocking=True)
+        for key in udata.keys():
+            if key != 'cell_id':
+                udata[key] = udata[key].to(device, non_blocking=True)
         masks_attention = masks_attention.to(device, non_blocking=True)
 
         # Collect cell IDs to join metadata
-        all_cell_ids.extend(udata[-1])
+        all_cell_ids.extend(udata['cell_id'])
 
         # Aggregate gene embeddings into cell and neighborhood
         # embeddings
-        ns_tokens = tokens[:, n_special_tokens:]
+        ns_tokens = udata['tokens'][:, n_special_tokens:]
 
         # Exclude masked tokens from aggregation
         if masked_tokens is not None:
             mask_indices = torch.isin(
-                tokens,
-                torch.tensor(masked_tokens, device=tokens.device)
+                udata['tokens'],
+                torch.tensor(masked_tokens, device=udata['tokens'].device)
                 ).unsqueeze(1).unsqueeze(1).expand(
-                    -1,-1, tokens.shape[-1], -1)
+                    -1,-1, udata['tokens'].shape[-1], -1)
             masks_attention = masks_attention.expand(
                 masks_attention.shape[0],
                 masks_attention.shape[1],
@@ -356,18 +354,12 @@ def infer(args: dict,
             for emb_layer in emb_layers:
                 cell_emb_list.append(return_layer_emb_fn(
                     layer=emb_layer,
-                    positions=positions,
-                    segments=segments,
-                    tokens=tokens,
-                    counts=counts,
+                    udata=udata,
                     masks_attention=masks_attention,
                     pad_neighborhood=True).cpu())
                 neighborhood_emb_list.append(return_layer_emb_fn(
                     layer=emb_layer,
-                    positions=positions,
-                    segments=segments,
-                    tokens=tokens,
-                    counts=counts,
+                    udata=udata,
                     masks_attention=masks_attention,
                     pad_neighborhood=False).cpu())
         

src/app/train.py
@@ -448,7 +448,8 @@ def train(args: dict,
 
         for itr, (udata, masks_enc, masks_pred, masks_attention) in enumerate(train_loader):
             for key in udata.keys():
-                udata[key] = udata[key].to(device, non_blocking=True)
+                if key != 'cell_id':
+                    udata[key] = udata[key].to(device, non_blocking=True)
             masks_enc = [u.to(device, non_blocking=True) for u in masks_enc]
             masks_pred = [u.to(device, non_blocking=True) for u in masks_pred]
             masks_attention = masks_attention.to(device, non_blocking=True)
@@ -462,7 +463,7 @@ def train(args: dict,
                 _new_lr = scheduler.step()
                 _new_wd = wd_scheduler.step()
 
-                def forward_target():
+                def forward_target(udata):
                     with torch.no_grad(): 
                         # no backward pass for target encoder
                         # Target encorder forward pass with output dim 
@@ -485,15 +486,14 @@ def train(args: dict,
 
                         return h
 
-                def forward_context():
+                def forward_context(udata):
                     # Context encoder forward pass with output dim (BATCH_SIZE,
                     # MIN_CONTEXT_SIZE, EMB_DIM) where MIN_CONTEXT_SIZE is
                     # minmum context size in the batch after removal of
                     # overlapping targets                     
-                    z, udata = encoder(
-                        udata=udata,
-                        masks=masks_enc,
-                        masks_attention=None)
+                    z, udata = encoder(udata=udata,
+                                       masks=masks_enc,
+                                       masks_attention=None)
 
                     # Predictor forward pass with output dim (BATCH_SIZE *
                     # N_TARGETS * N_CONTEXTS, TARGET_MASK_SIZE, EMB_DIM)
@@ -522,8 +522,8 @@ def train(args: dict,
                 # Step 1: forward pass
                 with torch.cuda.amp.autocast(dtype=torch.bfloat16,
                                              enabled=use_bfloat16):
-                    h = forward_target()
-                    z = forward_context()
+                    h = forward_target(udata)
+                    z = forward_context(udata)
                     loss = loss_fn(z, h)
 
                 # Step 2: backward pass and step

src/nichejepa/datasets/cell_datasets.py
@@ -94,7 +94,8 @@ class CellBaseDataset(Dataset):
         """
         for spc_tk in self.special_tokens:
             item_dict['tokens'] = item[f'{spc_tk}_token'] + item_dict['tokens']
-            item_dict['values'] = item[f'{spc_tk}_value'] + item_dict['values']
+            if self.gt_type != 'rank':
+                item_dict['values'] = item[f'{spc_tk}_value'] + item_dict['values']
             
         if self.gt_type != 'counts':
             # Add special token positions
@@ -290,9 +291,10 @@ class CellGraphDataset(CellBaseDataset):
             segment_seq_len=self.seq_len_cell)
         if self.gt_type != 'counts':
             item_dict['positions'] = [
-                position if tokens[i] != 0 else 0 for i, position in 
-                enumerate(list(range(1, len(tokens) + 1)))]
-        item_dict['segments'] = [1 if token != 0 else 0 for token in tokens]
+                position if item_dict['tokens'][i] != 0 else 0 for i, position in 
+                enumerate(list(range(1, len(item_dict['tokens']) + 1)))]
+        item_dict['segments'] = [
+            1 if token != 0 else 0 for token in item_dict['tokens']]
 
         # Get (sampled) gene tokens, positions, segments and values for
         # neighbor cell segments
@@ -312,13 +314,13 @@ class CellGraphDataset(CellBaseDataset):
                         [position if segment_tokens[i] != 0 else 0
                         for i, position in enumerate(
                             list(range(1, len(segment_tokens) + 1)))])
-                elif self.gt_type != 'rank':
+                if self.gt_type != 'rank':
                     item_dict['values'].extend(segment_values)
                 item_dict['tokens'].extend(segment_tokens)
                 item_dict['segments'].extend([segment if token != 0 else 0 for
                     token in segment_tokens])
 
-        if len(tokens) > (self.seq_len_cell + self.seq_len_neighborhood):
+        if len(item_dict['tokens']) > (self.seq_len_cell + self.seq_len_neighborhood):
             item_dict['tokens'] = item_dict['tokens'][
                 :self.seq_len_cell + self.seq_len_neighborhood]
             item_dict['segments'] = item_dict['segments'][
@@ -326,31 +328,38 @@ class CellGraphDataset(CellBaseDataset):
             if self.gt_type != 'counts':
                 item_dict['positions'] = item_dict['positions'][
                     :self.seq_len_cell + self.seq_len_neighborhood]
-            elif self.gt_type != 'rank':
+            if self.gt_type != 'rank':
                 item_dict['values'] = item_dict['values'][
                     :self.seq_len_cell + self.seq_len_neighborhood]
 
-        elif len(tokens) < (self.seq_len_cell + self.seq_len_neighborhood):
+        elif len(item_dict['tokens']) < (self.seq_len_cell + self.seq_len_neighborhood):
             item_dict['tokens'] += [0] * (
-                (self.seq_len_cell + self.seq_len_neighborhood) - len(tokens))
+                (self.seq_len_cell + self.seq_len_neighborhood) - len(item_dict['tokens']))
             item_dict['segments'] += [0] * (
-                (self.seq_len_cell + self.seq_len_neighborhood) - len(segments)
+                (self.seq_len_cell + self.seq_len_neighborhood) - len(item_dict['segments'])
                 )
             if self.gt_type != 'counts':
                 item_dict['positions'] += [0] * (
                     (self.seq_len_cell + self.seq_len_neighborhood) -
-                    len(positions))
-            elif self.gt_type != 'rank':
+                    len(item_dict['positions']))
+            if self.gt_type != 'rank':
                 item_dict['values'] += [0.0] * (
                     (self.seq_len_cell + self.seq_len_neighborhood) -
-                    len(values))
+                    len(item_dict['values']))
 
         # Add special tokens
         item_dict = self._add_special_seq(item=item,
                                           item_dict=item_dict)
 
+        none_keys = []
         for key in item_dict.keys():
-            item_dict[key] = torch.tensor(item_dict[key])
+            if item_dict[key] is not None:
+                item_dict[key] = torch.tensor(item_dict[key])
+            else:
+                none_keys.append(key)
+
+        for key in none_keys:
+            del(item_dict[key])
 
         item_dict['cell_id'] = item['cell_id']
         
@@ -398,17 +407,24 @@ class CellNeighborhoodDataset(CellBaseDataset):
                 range(1, len(gene_tokens_cell) + 1)) + list(
                 range(1, len(gene_tokens_neigh) + 1))
             item_dict['positions'] = [
-                position if tokens[i] != 0 else 0 for i, position in 
-                enumerate(positions)]
-        elif self.gt_type != 'rank':
+                position if item_dict['tokens'][i] != 0 else 0 for i, position in 
+                enumerate(item_dict['positions'])]
+        if self.gt_type != 'rank':
             item_dict['values'] = values_cell + values_neigh
 
         # Add special tokens
         item_dict = self._add_special_seq(item=item,
-                                            item_dict=item_dict)
+                                          item_dict=item_dict)
 
+        none_keys = []
         for key in item_dict.keys():
-            item_dict[key] = torch.tensor(item_dict[key])
+            if item_dict[key] is not None:
+                item_dict[key] = torch.tensor(item_dict[key])
+            else:
+                none_keys.append(key)
+
+        for key in none_keys:
+            del(item_dict[key])
 
         item_dict['cell_id'] = item['cell_id']
         

src/nichejepa/models/gene_transformers.py
@@ -683,15 +683,15 @@ class GeneTransformerCountEncoder(GeneTransformerBaseEncoder):
         # Get value embeddings
         if self.count_encoding == 'value_bins':
             value_emb_weights = self.value_emb_weights_projection(
-                udata['counts'].unsqueeze(dim=-1))
+                udata['values'].unsqueeze(dim=-1))
             value_emb = torch.matmul(
                 value_emb_weights, self.value_embed.weight)
-            zero_counts_mask = udata['counts'] == 0.0 # assign pad to 0 counts
+            zero_counts_mask = udata['values'] == 0.0 # assign pad to 0 counts
             zero_value_embed = self.special_value_embed(
                 torch.tensor(0, device=tokens.device)).to(value_emb.dtype)
             value_emb[zero_counts_mask] = zero_value_embed
         elif self.count_encoding == 'mlp':
-            value_emb = self.value_embed(udata['counts'].unsqueeze(dim=-1))           
+            value_emb = self.value_embed(udata['values'].unsqueeze(dim=-1))           
 
         # Add gene token and segment embeddings to value embeddings
         x = token_emb + seg_emb + value_emb
@@ -766,15 +766,15 @@ class GeneTransformerCountEncoder(GeneTransformerBaseEncoder):
         # Get value embeddings
         if self.count_encoding == 'value_bins':
             value_emb_weights = self.value_emb_weights_projection(
-                udata['counts'].unsqueeze(dim=-1))
+                udata['values'].unsqueeze(dim=-1))
             value_emb = torch.matmul(
                 value_emb_weights, self.value_embed.weight)
-            zero_counts_mask = udata['counts'] == 0.0 # assign padding to 0 counts
+            zero_counts_mask = udata['values'] == 0.0 # assign padding to 0 counts
             zero_value_embed = self.special_value_embed(
                 torch.tensor(0, device=tokens.device)).to(value_emb.dtype)
             value_emb[zero_counts_mask] = zero_value_embed
         elif self.count_encoding == 'mlp':
-            value_emb = self.value_embed(udata['counts'].unsqueeze(dim=-1))  
+            value_emb = self.value_embed(udata['values'].unsqueeze(dim=-1))  
 
         # Add gene token and segment embeddings to value embeddings
         x = token_emb + seg_emb + value_emb
@@ -933,15 +933,15 @@ class GeneTransformerCombinedEncoder(GeneTransformerBaseEncoder):
         # Get value embeddings
         if self.count_encoding == 'value_bins':
             value_emb_weights = self.value_emb_weights_projection(
-                udata['counts'].unsqueeze(dim=-1))
+                udata['values'].unsqueeze(dim=-1))
             value_emb = torch.matmul(
                 value_emb_weights, self.value_embed.weight)
-            zero_counts_mask = udata['counts'] == 0.0 # assign padding to 0 counts
+            zero_counts_mask = udata['values'] == 0.0 # assign padding to 0 counts
             zero_value_embed = self.special_value_embed(
                 torch.tensor(0, device=tokens.device)).to(value_emb.dtype)
             value_emb[zero_counts_mask] = zero_value_embed
         elif self.count_encoding == 'mlp':
-            value_emb = self.value_embed(udata['counts'].unsqueeze(dim=-1))         
+            value_emb = self.value_embed(udata['values'].unsqueeze(dim=-1))       
 
         # Add positional, segment, and gene embeddings to value embeddings
         x = pos_emb + seg_emb + token_emb + value_emb
@@ -1016,15 +1016,15 @@ class GeneTransformerCombinedEncoder(GeneTransformerBaseEncoder):
         # Get value embeddings
         if self.count_encoding == 'value_bins':
             value_emb_weights = self.value_emb_weights_projection(
-                udata['counts'].unsqueeze(dim=-1))
+                udata['values'].unsqueeze(dim=-1))
             value_emb = torch.matmul(
                 value_emb_weights, self.value_embed.weight)
-            zero_counts_mask = udata['counts'] == 0.0 # assign padding to 0 counts
+            zero_counts_mask = udata['values'] == 0.0 # assign padding to 0 counts
             zero_value_embed = self.special_value_embed(
                 torch.tensor(0, device=tokens.device)).to(value_emb.dtype)
             value_emb[zero_counts_mask] = zero_value_embed
         elif self.count_encoding == 'mlp':
-            value_emb = self.value_embed(udata['counts'].unsqueeze(dim=-1))  
+            value_emb = self.value_embed(udata['values'].unsqueeze(dim=-1))  
 
         # Add positional, segment, and gene embeddings to value embeddings
         x = pos_emb + seg_emb + token_emb + value_emb