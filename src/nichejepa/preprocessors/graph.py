c5278630317ca84119c04d16b81b612a280ca1af
sebastianbirk <sebastian.birk@outlook.com>
2026-01-28 17:51:06 +0000

    Add option to compute graph across multiple samples

--- src/nichejepa/preprocessors/graph.py
+++ src/nichejepa/preprocessors/graph.py
@@ -5,12 +5,65 @@ except:
     print("Could not import squidpy...")
 
 
+def _combine_neighbor_graphs(adata: ad.AnnData,
+                             n_neighs: int | None,
+                             radius: float | None,
+                             delaunay: bool,
+                             include_self_loop: bool,
+                             ) -> ad.AnnData:
+    """
+    Helper function to combine neighbor graphs computed using different
+    methods.
+    """
+    if n_neighs is not None:
+        # Compute knn spatial neighborhood graph
+        sq.gr.spatial_neighbors(adata,
+                                coord_type='generic',
+                                spatial_key='spatial',
+                                n_neighs=n_neighs,
+                                set_diag=include_self_loop,
+                                ) 
+        knn_conn = adata.obsp['spatial_connectivities']  
+    if radius is not None:
+        # Compute spatial neighborhood graph with radius
+        sq.gr.spatial_neighbors(adata,
+                                coord_type='generic',
+                                spatial_key='spatial',
+                                radius=radius,
+                                set_diag=include_self_loop,
+                                )
+        radius_conn = adata.obsp['spatial_connectivities'] 
+        if n_neighs is not None:
+            adata.obsp[
+                'spatial_connectivities'] = knn_conn.multiply(
+                    adata.obsp['spatial_connectivities'])
+    if delaunay:
+        # Compute spatial neighborhood graph with delaunay triangulation
+        sq.gr.spatial_neighbors(adata,
+                                coord_type='generic',
+                                spatial_key='spatial',
+                                delaunay=True,
+                                set_diag=include_self_loop,
+                                )
+        if n_neighs is not None:
+            adata.obsp[
+                'spatial_connectivities'] = knn_conn.multiply(
+                    adata.obsp['spatial_connectivities'])
+        if radius is not None:
+            adata.obsp[
+                'spatial_connectivities'] = radius_conn.multiply(
+                    adata.obsp['spatial_connectivities'])
+
+        return adata
+
+
 def construct_neighbor_graph(adata: ad.AnnData,
                              n_neighs: int | None = None,
                              radius: float | None = None,
                              delaunay: bool = True,
                              include_self_loop: bool = False,
-                             compute_neighbor_counts: bool = False
+                             batch_key: str | None = None,
+                             compute_neighbor_counts: bool = False,
                              ) -> ad.AnnData:
     """
     Compute neighbor graph and optionally aggregate cell features across
@@ -35,6 +88,11 @@ def construct_neighbor_graph(adata: ad.AnnData,
         intersection neighborhood graph will be computed.
     include_self_loop:
         If 'True', include cell itself in neighborhood graph.
+    batch_key:
+        Key in adata.obs where the batch is stored in case the AnnData
+        contains multiple batches. This is important to specify if there
+        are multiple batches in the AnnData to perform separate neighbor
+        graph computation and then concatenate.
     compute_neighbor_counts:
         If 'True', aggregate counts across neighborhood and store in
         `adata.layers['X_neighborhood']`.
@@ -45,45 +103,38 @@ def construct_neighbor_graph(adata: ad.AnnData,
         AnnData object with aggregated counts available in
         `adata.layers['X_neighborhood']`.
     """
-    if n_neighs is not None:
-        # Compute knn spatial neighborhood graph
-        sq.gr.spatial_neighbors(adata,
-                                coord_type='generic',
-                                spatial_key='spatial',
-                                n_neighs=n_neighs,
-                                set_diag=include_self_loop,
-                                ) 
-        knn_connectivities = adata.obsp['spatial_connectivities']  
-    if radius is not None:
-        # Compute spatial neighborhood graph with radius
-        sq.gr.spatial_neighbors(adata,
-                                coord_type='generic',
-                                spatial_key='spatial',
-                                radius=radius,
-                                set_diag=include_self_loop,
-                                )
-        radius_connectivities = adata.obsp['spatial_connectivities'] 
-        if n_neighs is not None:
-            adata.obsp[
-                'spatial_connectivities'] = knn_connectivities.multiply(
-                    adata.obsp['spatial_connectivities'])
-    
-    if delaunay:
-        # Compute spatial neighborhood graph with delaunay triangulation
-        sq.gr.spatial_neighbors(adata,
-                                coord_type='generic',
-                                spatial_key='spatial',
-                                delaunay=True,
-                                set_diag=include_self_loop,
-                                )
-        if n_neighs is not None:
-                adata.obsp[
-                    'spatial_connectivities'] = knn_connectivities.multiply(
-                        adata.obsp['spatial_connectivities'])
-        if radius is not None:
-                adata.obsp[
-                    'spatial_connectivities'] = radius_connectivities.multiply(
-                        adata.obsp['spatial_connectivities'])            
+    if batch_key is not None:
+        batches = adata.obs[batch_key].unique()
+        adata_batch_list = []
+
+        for batch in batches:
+            print(f"\nProcessing batch {batch}...")
+            print("Loading data...")
+            adata_batch = adata[adata.obs[batch_key] == batch]
+
+            print("Computing spatial neighborhood graph...")
+            adata_batch = _combine_neighbor_graphs(
+                adata_batch,
+                n_neighs,
+                radius,
+                delaunay,
+                include_self_loop)
+            adata_batch_list.append(adata_batch)
+
+        adata = ad.concat(adata_batch_list, join="inner")
+
+        # Block-diagonal concatenate graphs (disconnected components)
+        adata.obsp['spatial_connectivities'] = sp.block_diag(
+            [a.obsp['spatial_connectivities'] for a in adata_batch_list],
+            format="csr",
+        )    
+    else:
+        adata = _combine_neighbor_graphs(
+            adata,
+            n_neighs,
+            radius,
+            delaunay,
+            include_self_loop)           
     
     if compute_neighbor_counts:
         adata.layers['X_neighborhood'] = (
